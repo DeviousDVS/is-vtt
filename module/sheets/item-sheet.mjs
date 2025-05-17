638b82971e3d560e342493768e717d987883a60e
DeviousDVS <dvs@internode.on.net>
2025-05-17 12:34:45 +0930

    updated function call for Foundry v12

--- module/sheets/item-sheet.mjs
+++ module/sheets/item-sheet.mjs
@@ -3,61 +3,66 @@
  * @extends {ItemSheet}
  */
 export class InvisibleSunItemSheet extends ItemSheet {
-
-  /** @override */
-  static get defaultOptions() {
-    return mergeObject(super.defaultOptions, {
-      classes: ["invisiblesun", "sheet", "item"],
-      width: 520,
-      height: 480,
-      tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description" }]
-    });
-  }
-
-  /** @override */
-  get template() {
-    const path = "systems/invisible-sun/templates/item";
-    // Return a single sheet for all item types.
-    // return `${path}/item-sheet.html`;
-
-    // Alternatively, you could use the following return statement to do a
-    // unique item sheet by type, like `weapon-sheet.html`.
-    return `${path}/item-${this.item.type}-sheet.html`;
-  }
-
-  /* -------------------------------------------- */
-
-  /** @override */
-  getData() {
-    // Retrieve base data structure.
-    const context = super.getData();
-
-    // Use a safe clone of the item data for further operations.
-    const itemData = context.item;
-
-    // Retrieve the roll data for TinyMCE editors.
-    context.rollData = {};
-    let actor = this.object?.parent ?? null;
-    if (actor) {
-      context.rollData = actor.getRollData();
+    /** @override */
+    static get defaultOptions() {
+        return foundry.utils.mergeObject(super.defaultOptions, {
+            classes: ["invisiblesun", "sheet", "item"],
+            width: 520,
+            height: 480,
+            tabs: [
+                {
+                    navSelector: ".sheet-tabs",
+                    contentSelector: ".sheet-body",
+                    initial: "description",
+                },
+            ],
+        });
     }
 
-    // Add the actor's data to context.data for easier access, as well as flags.
-    context.system = itemData.system;
-    context.flags = itemData.flags;
+    /** @override */
+    get template() {
+        const path = "systems/invisible-sun/templates/item";
+        // Return a single sheet for all item types.
+        // return `${path}/item-sheet.html`;
 
-    return context;
-  }
+        // Alternatively, you could use the following return statement to do a
+        // unique item sheet by type, like `weapon-sheet.html`.
+        return `${path}/item-${this.item.type}-sheet.html`;
+    }
 
-  /* -------------------------------------------- */
+    /* -------------------------------------------- */
 
-  /** @override */
-  activateListeners(html) {
-    super.activateListeners(html);
+    /** @override */
+    getData() {
+        // Retrieve base data structure.
+        const context = super.getData();
 
-    // Everything below here is only needed if the sheet is editable
-    if (!this.isEditable) return;
+        // Use a safe clone of the item data for further operations.
+        const itemData = context.item;
 
-    // Roll handlers, click handlers, etc. would go here.
-  }
+        // Retrieve the roll data for TinyMCE editors.
+        context.rollData = {};
+        let actor = this.object?.parent ?? null;
+        if (actor) {
+            context.rollData = actor.getRollData();
+        }
+
+        // Add the actor's data to context.data for easier access, as well as flags.
+        context.system = itemData.system;
+        context.flags = itemData.flags;
+
+        return context;
+    }
+
+    /* -------------------------------------------- */
+
+    /** @override */
+    activateListeners(html) {
+        super.activateListeners(html);
+
+        // Everything below here is only needed if the sheet is editable
+        if (!this.isEditable) return;
+
+        // Roll handlers, click handlers, etc. would go here.
+    }
 }
