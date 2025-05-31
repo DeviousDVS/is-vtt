3632ec662a25ded45a5465852ff4fcd8df7b6d74
DeviousDVS <dvs@internode.on.net>
2025-05-31 17:52:34 +0930

    added testament partial

--- module/helpers/templates.mjs
+++ module/helpers/templates.mjs
@@ -3,13 +3,13 @@
  * Pre-loaded templates are compiled and cached for fast access when rendering
  * @return {Promise}
  */
- export const preloadHandlebarsTemplates = async function() {
-  return loadTemplates([
-
-    // Actor partials.
-    "systems/invisible-sun/templates/actor/parts/actor-features.html",
-    "systems/invisible-sun/templates/actor/parts/actor-items.html",
-    "systems/invisible-sun/templates/actor/parts/actor-spells.html",
-    "systems/invisible-sun/templates/actor/parts/actor-effects.html",
-  ]);
+export const preloadHandlebarsTemplates = async function () {
+    return loadTemplates([
+        // Actor partials.
+        "systems/invisible-sun/templates/actor/parts/actor-features.html",
+        "systems/invisible-sun/templates/actor/parts/actor-items.html",
+        "systems/invisible-sun/templates/actor/parts/actor-spells.html",
+        "systems/invisible-sun/templates/actor/parts/actor-effects.html",
+        "systems/invisible-sun/templates/actor/parts/actor-testament.html",
+    ]);
 };
